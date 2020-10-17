04195c213a2868f1fc43fbe63ceca9d5c48990b2
mishapodl <mishapod@gmail.com>
2020-10-17 23:40:07 +0300

    realized basic login google

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.scss';
+import { GoogleButton } from './components/GoogleButton';
+
+const App = () => {
+  return <GoogleButton />;
+};
+
+export default App;
